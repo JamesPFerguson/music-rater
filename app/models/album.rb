8c5b815cdd3b1c3da19b437b774f633caba2ea54
James Ferguson <ncjamesferguson@gmail.com>
2018-08-09 00:53:34 +0000

    add model relations

--- app/models/album.rb
+++ app/models/album.rb
@@ -1,2 +1,4 @@
 class Album < ActiveRecord::Base
+  has_many :reviews, through: :album_reviews
+  has_many :album_reviews
 end
--- app/models/album_review.rb
+++ app/models/album_review.rb
@@ -1,2 +1,4 @@
 class AlbumReview < ActiveRecord::Base
+  belongs_to :album
+  belongs_to :review
 end
