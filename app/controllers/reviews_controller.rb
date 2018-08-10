ca03b60e8904fd3da9939f62fc7d4c812a858e5b
JamesPFerguson <ncjamesferguson@gmail.com>
2018-08-09 20:58:33 -0400

    Update reviews_controller.rb

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,2 +1,23 @@
 class ReviewsController < ApplicationController
+  
+  def create
+    
+  end
+  
+  def update
+    
+  end
+  
+  def destroy
+    
+  end
+  
+  def show
+    
+  end
+  
+  def index
+    
+  end
+  
 end
