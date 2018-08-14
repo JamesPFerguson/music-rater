e693a2b62aa706de587b14aa12c2b9eadec64301
jamespferguson <ncjamesferguson@gmail.com>
2018-08-14 13:21:50 -0400

    remove password from users

--- db/migrate/20180809004713_create_users.rb
+++ db/migrate/20180809004713_create_users.rb
@@ -2,8 +2,6 @@ class CreateUsers < ActiveRecord::Migration[4.2]
   def change
     create_table :users do |t|
       t.string :username
-      t.string :email
-      t.string :password_digest
       t.timestamps null: false
     end
   end
