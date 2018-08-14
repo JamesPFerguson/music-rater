ebdc121fbfe7fe9ababd0fcd915159a3ad53559c
jamespferguson <ncjamesferguson@gmail.com>
2018-08-14 13:08:56 -0400

    create store, add redux

---
+++ client/src/reducers/index.js
@@ -0,0 +1,6 @@
+// import { combineReducers } from 'redux';
+// import reviewReducer from './reviewReducer';
+
+// export const rootReducer = combineReducers({
+//     reviw: reviewReducer,
+//   })
