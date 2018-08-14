3812de56c32f6dc982aa67531fde0a06a52748e9
jamespferguson <ncjamesferguson@gmail.com>
2018-08-14 03:49:00 -0400

    add experimental css

--- client/src/components/artists.js
+++ client/src/components/artists.js
@@ -15,7 +15,7 @@ export default class Artist extends React.Component {
 
     render() {
         return (
-        <div> Hi </div>
+        <div> </div>
         )
     }
 
--- client/src/components/review.js
+++ client/src/components/review.js
@@ -1,27 +1,23 @@
 import React from 'react';
 export default class Review extends React.Component {
 
-  componentWillMount() {
-    debugger
-  }
-
   render() {
     return (
       
-      <div className="review">
-      <div className="review-album-title">
+      <span className="review">
 
+      <span className="review-album-title">
       <a href={`/albums/${this.props.album.id}`}>{this.props.album.title}</a>
-       by: <a href={`/artists/${this.props.album.artist_id}`}>{this.props.album.artist_name}</a> -- {this.props.rating}/5 
-       </div>
+        by: <a href={`/artists/${this.props.album.artist_id}`}>{this.props.album.artist_name}</a> -- {this.props.rating}/5 
+       </span>
       <br/>
       <br/>
-      <img src={this.props.album.art_url} alt="album art"/>
-      <div className="review-user">
+      <img className="review-album-art" src={this.props.album.art_url} alt="album art"/>
+      <span className="review-user">
       Reviewed by: <a href={`/users/${this.props.user.id}`}>{this.props.user.username}</a>
-      </div>
-      <div className="review-text">{this.props.content}</div>
-      </div>
+      </span>
+      <span className="review-text">{this.props.content}</span>
+      </span>
     )
   }
 
