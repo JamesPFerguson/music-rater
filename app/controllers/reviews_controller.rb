acfdfbf8c1b08cc5a01c23f5958e1f9c5ca83428
jamespferguson <ncjamesferguson@gmail.com>
2018-08-16 03:46:55 -0400

    build some of create review rails action

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -2,6 +2,12 @@ class ReviewsController < ApplicationController
   
   def create
     byebug
+    user = User.create(username: params[:username])
+    artist = Artist.find_or_create_by(name: params[:artist_name])
+    album = Album.create(title: params[:album], art_url: params[:art_url],
+       artist_name: params[:artist_name])
+    artist.albums << album
+    
   end
   
   def update
