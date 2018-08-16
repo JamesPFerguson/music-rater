65c4c42746a3bbdf132162c01a77ef62012bd85c
jamespferguson <ncjamesferguson@gmail.com>
2018-08-15 22:47:03 -0400

    comment provider back in, get form to reset after submit

--- client/src/components/reviewForm.js
+++ client/src/components/reviewForm.js
@@ -2,6 +2,7 @@ import React from 'react';
 
 export default class ReviewForm extends React.Component {
 
+
     constructor(props) {
         super();
 
@@ -15,8 +16,14 @@ export default class ReviewForm extends React.Component {
             submitted: false
         }
 
+        this.baseState = this.state
+
         this.handleChange = this.handleChange.bind(this);
         this.handleSubmit = this.handleSubmit.bind(this);
+    }
+
+    resetForm = () => {
+        this.setState(this.baseState)
     }
 
     handleChange = (e) => {
@@ -27,12 +34,15 @@ export default class ReviewForm extends React.Component {
     handleSubmit = (e) => {
         e.preventDefault();
 
-        // this.setState({ submitted: true });
+        // let reviewData = Object.assign({}, this.state)
+        // this.props.addReview(this.state)
+        // for (const key of Object.keys(this.refs)) {
+        //     this.refs[key].value = '';
+        // }
+        this.resetForm();
+       
 
-        debugger
-        // const { dispatch } = this.props;
 
-        // dispatch(userActions.login(username, password))
     }
 
     render() {
@@ -44,20 +54,20 @@ export default class ReviewForm extends React.Component {
             <form name="add-review" onSubmit={this.handleSubmit}>
             <br/><br/>
             <label>Username: </label>
-            <input type="text" name="username" value={username} onChange={this.handleChange} />
+            <input type="text" ref="username" name="username" value={username} onChange={this.handleChange} />
             <br/><br/>
             <label>Album: </label>
-            <input type="text" name="album" value={album} onChange={this.handleChange} />
+            <input type="text" ref="album" name="album" value={album} onChange={this.handleChange} />
             <br/><br/>
             <label>Artist: </label>
-            <input type="text" name="artist_name" value={artist_name} onChange={this.handleChange} />
+            <input type="text" ref="artist_name" name="artist_name" value={artist_name} onChange={this.handleChange} />
             <br/><br/>
             <label>Rating: </label>
-            <input type="text" name="rating" value={rating} onChange={this.handleChange} />
+            <input type="text" ref="rating" name="rating" value={rating} onChange={this.handleChange} />
             <br/><br/>
             <label>Review: </label>
             <br/>
-            <textarea rows="8" cols="80" name="content" value={content} onChange={this.handleChange} />
+            <textarea rows="8" cols="80"  ref="content" name="content" value={content} onChange={this.handleChange} />
             <br/><br/>
             <input type="submit"/>
 
--- client/src/index.js
+++ client/src/index.js
@@ -8,7 +8,7 @@ import store from './store.js';
 
 ReactDOM.render(
     <Provider store={store}>
-    <App />,
+        <App />
     </Provider>,
     document.getElementById('root')
 );
