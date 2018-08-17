cceb4cada6ba951cd4b9247445088e496d18bac8
jamespferguson <ncjamesferguson@gmail.com>
2018-08-17 15:11:26 -0400

    delete review container and loading page component

--- client/src/components/loadingPage.js
+++
@@ -1,9 +0,0 @@
-import React from 'react';
-
-export default class LoadingPage extends React.Component {
-    render() {
-        return (
-            <div> You are seeing this because the reviews have yet to load in. </div>
-        )
-    }
-}
