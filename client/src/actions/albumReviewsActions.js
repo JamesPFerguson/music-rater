7fb41b8e8dab581aa52d0730eb2f827165afbeb2
jamespferguson <ncjamesferguson@gmail.com>
2018-08-18 12:38:31 -0400

    change actions to use dynamic routing

--- client/src/actions/albumReviewsActions.js
+++ client/src/actions/albumReviewsActions.js
@@ -1,6 +1,6 @@
 export function fetchAlbumReviews(id) {
     return (dispatch) => {
-        return fetch(`http://localhost:3000/albums/1`)
+        return fetch(`http://localhost:3000/albums/${id}`)
             .then(res => res.json())
             .then(response => {dispatch({type: 'FETCH_ALBUM_REVIEWS', album_reviews: response})}
         )
--- client/src/actions/artistActions.js
+++ client/src/actions/artistActions.js
@@ -1,6 +1,6 @@
 export function fetchArtistAlbums(id) {
     return (dispatch) => {
-        return fetch(`http://localhost:3000/artists/1`)
+        return fetch(`http://localhost:3000/artists/${id}`)
             .then(res => res.json())
             .then(response => {dispatch({type: 'FETCH_ARTIST_ALBUMS', artist_albums: response})}
         )
