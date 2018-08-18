16d2bee39317a174e9e1b25dac8d0499530d6c32
jamespferguson <ncjamesferguson@gmail.com>
2018-08-17 23:34:32 -0400

    add album reviews and artist route to app

--- client/src/App.js
+++ client/src/App.js
@@ -7,6 +7,7 @@ import { BrowserRouter as Router, Route } from 'react-router-dom';
 import ReviewForm from './components/reviewForm';
 import ReviewsListContainer from './containers/reviewsListContainer';
 import Home from './components/home'
+import AlbumReviewsContainer from './containers/albumReviewsContainer';
 
 class App extends Component {
   render() {
@@ -14,6 +15,8 @@ class App extends Component {
       <Router>
          <div className="App">
             <Route exact path="/" component={Home} />
+            <Route exact path="/albums/:id" component={AlbumReviewsContainer} />
+            <Route exact path="/artists/:id" component={ArtistContainer} />
          </div>
        </Router>
     );
