58e4a27b4b44b2d3a1c849b6447225126c3075d0
jamespferguson <ncjamesferguson@gmail.com>
2018-08-23 16:59:33 -0400

    get default image to render onerror, remove unused span tag in user component

--- client/src/components/album.js
+++ client/src/components/album.js
@@ -5,21 +5,14 @@ export default class Album extends React.Component {
 
     render() {
         if (this.props.album !== null) {
-            let albumArt = null
-            if (this.props.album.art_url === "") {
-                albumArt = <img className="review-album-art" src="https://media2.fishtank.my/app_themes/raaga/assets/images/default-album-art.png" alt="album art"/>
-            }
-            else {
-                albumArt = <img className="review-album-art" src={this.props.album.art_url} 
-                    onError="this.onError=null;this.src='https://media2.fishtank.my/app_themes/raaga/assets/images/default-album-art.png'"
-                    alt="album art"/>
-            }
                 return (
                     <div className="">
                         <strong><a href={`/albums/${this.props.album.id}`}>{this.props.album.title}</a> by: </strong><label className="artist-name">
                         <a href={`/artists/${this.props.album.artist_id}`}>{this.props.album.artist_name}</a></label>
                         <br/>
-                        {albumArt}
+                        <img className="review-album-art" src={this.props.album.art_url} 
+                         onError={(e)=>{e.target.src="https://media2.fishtank.my/app_themes/raaga/assets/images/default-album-art.png", e.target.onError=null} }
+                         alt="album art"/>
                     </div>
                 )
         } else {
