5dc01a1fea7c144f9b5c089a0e610abb7006d884
jamespferguson <ncjamesferguson@gmail.com>
2018-08-23 18:55:49 -0400

    clean up review form css

--- client/src/components/ReviewList/ReviewForm.js
+++ client/src/components/ReviewList/ReviewForm.js
@@ -43,50 +43,49 @@ class ReviewForm extends React.Component {
     render() {
         const {username, album, art_url, artist_name, rating, content} = this.state;
         return (
-            <div className="review-form-container mx-auto">
-                <div> <h3 className="col-sm-4"> Add a review </h3> <br/>
+            <div className="review-form-container ">
+                <div> <h3 className=""> Add a review </h3> <br/>
                     <div className="review-form">
                         <form name="add-review" onSubmit={this.handleSubmit}>
                             <div className="form-group row">
                                 <label className="col-2 col-form-label">Username: </label>
-                                <div className="col-sm-2">
+                                <div className="col-2">
                                     <input type="text" size="25" className="form-control" ref="username" name="username" value={username} onChange={this.handleChange} />
                                 </div>
                             </div>
                             <div className="form-group row">
                                 <label className="col-2 col-form-label">Album: </label>
-                                <div className="col-sm-2">
+                                <div className="col-2">
                                     <input type="text" className="form-control" ref="album" name="album" value={album} onChange={this.handleChange} />
                                 </div>
                             </div>
                             <div className="form-group row">
                                 <label className="col-2 col-form-label">Album Art Image URL:</label>
-                                <div className="col-sm-5">
+                                <div className="col-4">
                                     <input type="text" className="form-control" ref="art_url" name="art_url" value={art_url} onChange={this.handleChange} />
                                 </div>
                             </div>
                             <div className="form-group row">
                                 <label className="col-2 col-form-label">Artist: </label>
-                                <div className="col-sm-2">
+                                <div className="col-3">
                                     <input type="text" className="form-control" ref="artist_name" name="artist_name" value={artist_name} onChange={this.handleChange} />
                                 </div>
                             </div>
                             <div className="form-group row">
                                 <label className="col-2 col-form-label">Rating: </label>
-                                <div className="col-sm-1">
-                                    <input type="text" className="form-control" ref="rating" name="rating" value={rating} onChange={this.handleChange} />
+                                <div className="col-1">
+                                    <input type="text" size="5" className="form-control" ref="rating" name="rating" value={rating} onChange={this.handleChange} />
                                 </div>
                             </div>
                             <div className="form-group row">
                                 <label className="col-2 col-form-label">Review: </label>
-                                <div className="col-sm-10">
-                                    <textarea rows="8" cols="80" className="form-control" ref="content" name="content" value={content} onChange={this.handleChange} />
+                                <div className="col-6">
+                                    <textarea rows="8" cols="50" className="col form-control" ref="content" name="content" value={content} onChange={this.handleChange} />
                                 </div>
                             </div>
-                            <div className="form-group row">
-                                <div className="col-sm-7">
+                            <div className="form-group row justify-content-center">
+                                <div className="p-3">
                                 <input type="submit" name="Add Review" value="Add Review" />
-                                <br/><br/>
                                 </div>
                             </div>
 
